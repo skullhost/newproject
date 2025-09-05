8e021c31c33ae67c53ca6320fa7b7e6e2c89562e
skullhost <skulltiktok21@gmail.com>
2025-09-05 17:38:21 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
 const supabaseUrl = "https://zvqlsgwccrdqjgcxgmzq.supabase.co";
-const supabaseKey = "YOUR_ANON_KEY";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2cWxzZ3djY3JkcWpnY3hnbXpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwNTc0MDUsImV4cCI6MjA3MjYzMzQwNX0.6Ge1ON_x9Ce-l4tFRtH_Ks9o3v1RouLIDejtbohjo4Y";
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 async function loadProductsAdmin(){
